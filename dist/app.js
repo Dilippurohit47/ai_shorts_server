dbfe10d1daae34404320590bbeccf5e51d8176b2
Dilippurohit47 <dilippurohit204@gmail.com>
2026-05-16 16:14:41 +0530

    add prod origin in cors

--- dist/app.js
+++ dist/app.js
@@ -31,7 +31,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.static(path_1.default.join(__dirname, "..")));
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://dilip-purohit.xyz:3000"],
     credentials: true
 }));
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "..")))
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","http://dilip-purohit.xyz:3000"],
   credentials:true
 }));
 
